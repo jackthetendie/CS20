4845cba00665b9347bc1ea7cb97c50b9ee314e23
510332001 <510332001@L815WD220023.edu.cbe.ab.ca>
2026-09-14 11:41:25 -0600

    11:41am

--- chapter3/src/skillbuilder/rectangle.java
+++ chapter3/src/skillbuilder/rectangle.java
@@ -10,7 +10,8 @@ public class rectangle {
 	     //declare variables
 		int length;
 		int width; 
-		
+		int area;
+		int perimeter;
 		//Jack
 		
 	    //create a scanner object
@@ -34,10 +35,19 @@ public class rectangle {
 		
 		//display the width and length on the console
 		System.out.println("width is: " + width);
-		System.out.print("length is: " + length);
+		System.out.println("length is: " + length);
 		
+		//calculate the area
+		area = length * width;
 		
+		//display the area on the screen
+		System.out.println("Area = "+ area);
 		
+		//calculate the perimeter
+		perimeter = (2 * length + 2 * width);
+				
+		//display the perimeter on the screen
+		System.out.println("perimeter = "+ perimeter);
 	}
 
 }
